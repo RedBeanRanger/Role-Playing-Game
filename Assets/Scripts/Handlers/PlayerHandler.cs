a9864da50a96e5ea276537d92d041c714d874f87
Angela Xin Zhang <angelaxinz@gmail.com>
2022-03-13 22:18:10 -0400

    Added Comment

--- Assets/Scripts/Handlers/PlayerHandler.cs
+++ Assets/Scripts/Handlers/PlayerHandler.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerHandler : MonoBehaviour
-{
+{#comment
     public bool CharacterSwitched = false;
     public Animator HudAnim0;
     public Animator HudAnim1;
